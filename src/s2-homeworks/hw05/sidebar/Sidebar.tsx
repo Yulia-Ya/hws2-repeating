1277354e62dc104c421ab5d24a695eca4a33091b
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-08-16 17:55:21 +0300

    task 5.1

--- src/s2-homeworks/hw05/sidebar/Sidebar.tsx
+++ src/s2-homeworks/hw05/sidebar/Sidebar.tsx
@@ -26,7 +26,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-pre-junior-link"}
             to={PATH.PRE_JUNIOR}
             onClick={handleClose}
-            className={(isActive) => (isActive ? s.active : "")}
+            className={({isActive}) => (isActive ? s.active : "")}
           >
             Pre-junior
           </NavLink>
@@ -34,7 +34,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-junior-link"}
             to={PATH.JUNIOR}
             onClick={handleClose}
-            className={(isActive) => (isActive ? s.active : "")}
+            className={({isActive}) => (isActive ? s.active : "")}
           >
             Junior
           </NavLink>
@@ -42,7 +42,7 @@ export const Sidebar: FC<PropsType> = ({ open, handleClose }) => {
             id={"hw5-junior-plus-link"}
             to={PATH.JUNIOR_PLUS}
             onClick={handleClose}
-            className={(isActive) => (isActive ? s.active : "")}
+            className={({isActive}) => (isActive ? s.active : "")}
           >
             Junior Plus
           </NavLink>

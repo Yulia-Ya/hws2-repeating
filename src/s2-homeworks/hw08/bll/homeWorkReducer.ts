e8e42292913b5214b6cfc6fe5a8c11e656a60688
Yulia Yakusheva <108211145+Yulia-Ya@users.noreply.github.com>
2024-08-23 18:34:47 +0300

    filtered reducer

--- src/s2-homeworks/hw08/bll/homeWorkReducer.ts
+++ src/s2-homeworks/hw08/bll/homeWorkReducer.ts
@@ -1,20 +1,29 @@
-import {UserType} from '../HW8'
+import { UserType } from "../HW8";
 
 type ActionType =
-    | { type: 'sort'; payload: 'up' | 'down' }
-    | { type: 'check'; payload: number }
+  | { type: "sort"; payload: "up" | "down" }
+  | { type: "check"; payload: number };
 
-export const homeWorkReducer = (state: any, action: any): any => { // need to fix any
-    switch (action.type) {
-        case 'sort': { // by name
-
-            return state // need to fix
-        }
-        case 'check': {
-
-            return state // need to fix
-        }
-        default:
-            return state
+export const homeWorkReducer = (
+  state: UserType[],
+  action: ActionType
+): UserType[] => {
+  // need to fix any
+  switch (action.type) {
+    case "sort": {
+      // by name
+    //   if (action.payload === "up") {
+    //     return [...state].sort((a, b) => (a.name > b.name ? 1 : -1));
+    //   } else {
+    //     return [...state].sort((a, b) => (a.name < b.name ? 1 : -1));
+    //   }
+    const newState = [...state].sort((a, b) => (a.name > b.name ? 1 : -1));
+    return action.payload === 'up' ? newState : newState.reverse()
     }
-}
+    case "check": {
+      return state.filter(el=> el.age > action.payload)
+    }
+    default:
+      return state;
+  }
+};
